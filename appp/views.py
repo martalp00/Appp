a46088908d93e581c0e47a8634edf58b05d05dca
martalp00 <martalo1304@gmail.com>
2023-04-26 23:26:46 +0200

    Vistas y busquedas de canciones añadidas

--- appp/views.py
+++ appp/views.py
@@ -1,17 +1,27 @@
 """
 Este módulo contiene las vistas para la aplicación web "Appp"
 """
+import requests
+
 from django.shortcuts import render
 
+def search(request):
+    return render(request, 'search.html')
 
-def homepage(request):
-    """
-    Renderiza la página de inicio.
+def search_results(request):
+    artist = request.GET.get('artist')
+    url = f'https://ws.audioscrobbler.com/2.0/?method=artist.search&artist={artist}&api_key=6364e51db1e7ec45657136ed75292af0&format=json'
+    response = requests.get(url)
+    data = response.json()
+    artists = data['results']['artistmatches']['artist']
+    if artists:
+        return render(request, 'search_results.html', {'artists': artists})
+    else:
+        return render(request, 'search_results.html', {'error': 'No artist found.'})
 
-    Args:
-        request: La solicitud HTTP entrante.
-
-    Returns:
-        La respuesta HTTP que contiene la página de inicio renderizada.
-    """
-    return render(request, 'homepage.html')
+def albums(request, artist_name):
+    url = f'https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist={artist_name}&api_key=6364e51db1e7ec45657136ed75292af0&format=json'
+    response = requests.get(url)
+    data = response.json()
+    albums = data['topalbums']['album']
+    return render(request, 'albums.html', {'albums': albums})
